6d74d756ce4e41579699e3da851791d1492c9eca
liar <794763733@qq.com>
2019-01-16 18:07:58 +0800

    初始化项目，增加数据库表信息

---
+++ ChangHeWebSite/Areas/ManagementSystem/Models/SystemInfo.cs
@@ -0,0 +1,25 @@
+﻿namespace ChangHeWebSite.Areas.ManagementSystem.Models
+{
+    /// <summary>
+    /// 系统信息
+    /// </summary>
+    public class SystemInfo
+    {
+        /// <summary>
+        /// 系统名称
+        /// </summary>
+        public string SystemName { get; set; }
+        /// <summary>
+        /// 系统Logo
+        /// </summary>
+        public string SystemLogo { get; set; }
+        /// <summary>
+        /// 系统版权
+        /// </summary>
+        public string SystemCopyright { get; set; }
+        /// <summary>
+        /// 系统描述
+        /// </summary>
+        public string SystemDescription { get; set; }
+    }
+}
