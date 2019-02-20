c688dc1e360b75898a057dafe7ad94cc1e5b39fe
liar <794763733@qq.com>
2019-02-20 15:31:44 +0800

    留言功能完成

--- ChangHeWebSite/Controllers/IndexController.cs
+++ ChangHeWebSite/Controllers/IndexController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using ChangHeWebSite.Areas.Admin.Base.Database;
 using ChangHeWebSite.Areas.Admin.Models;
+using ChangHeWebSite.Areas.Admin.Models.Dto;
 using ChangHeWebSite.Models;
 using ChangHeWebSite.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
@@ -33,6 +34,19 @@ namespace ChangHeWebSite.Controllers
             dto.Newses = _db.Newses.Take(4).ToList();
             dto.Covers = Mapper.Map<List<FrontImageDto>>(_db.Images.Where(i => i.ImageType == ImageType.Cover).ToList());
             return View(dto);
+        }
+        /// <summary>
+        /// 插入一条信息数据
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public JsonResult InsertMessage(Message message)
+        {
+            ResponseTemplate response = new ResponseTemplate();
+            _db.Messages.Add(message);
+            response.Data = _db.SaveChanges();
+            response.Success = true;
+            return Json(response);
         }
     }
 }
